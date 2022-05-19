3d227e01e646baf2d0d2defb091b85736e64ecd1
Sergio Follyvi <62280198+serfoll@users.noreply.github.com>
2022-05-19 16:44:37 +0200

    save users, no duplicates and get all users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,12 +13,23 @@ let User = require("../models/user.model");
 //add new user
 router.route("/").post((req, res) => {
   const { username } = req.body;
+
   const newUser = new User({ username });
 
   newUser
     .save()
     .then(() => res.json({ username: newUser.username, id: newUser.id }))
-    .catch((err) => res.status(400).json(`Error ${err}`));
+    .catch((err) => {
+      if ((err.code = 11000))
+        res.json({ username: newUser.username, id: newUser.id });
+    });
+});
+
+//get all users
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.send([users]))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 //export router
