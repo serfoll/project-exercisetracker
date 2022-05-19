58b2d70a5bd0d57060095b54262842bf55c9f57f
Sergio Follyvi <62280198+serfoll@users.noreply.github.com>
2022-05-19 16:53:43 +0200

    fixed response message

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,25 +3,20 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
-//get all users
-// router.route("/").get((req, res) => {
-//   User.find()
-//     .then((users) => res.json(users))
-//     .catch((err) => res.status(400).json(`Eroor ${err}`));
-// });
-
 //add new user
 router.route("/").post((req, res) => {
   const { username } = req.body;
 
   const newUser = new User({ username });
 
+  const jsonRes = { username: newUser.username, _id: newUser.id };
+
   newUser
     .save()
-    .then(() => res.json({ username: newUser.username, id: newUser.id }))
+    .then(() => res.json(jsonRes))
     .catch((err) => {
-      if ((err.code = 11000))
-        res.json({ username: newUser.username, id: newUser.id });
+      if ((err.code = 11000)) res.json(jsonRes);
+      else res.status(400).json(`Error: ${err}`);
     });
 });
 
